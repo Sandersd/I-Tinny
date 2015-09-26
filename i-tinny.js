b948cb9fdfd4c6980823a635e9f7d5929c0cdf65
Lorraine Chan <lorrainechan@Lorraines-MacBook-Pro.local>
2015-09-26 14:52:19 -0700

    Set session assessment id

--- i-tinny.js
+++ i-tinny.js
@@ -6,16 +6,24 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.body.events({
+  Template.assessment.events({
     "click .new-assessment": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
 
       HTTP.call('POST', 'https://api-sandbox.traitify.com/v1/assessments', {headers: { "Content-Type": "application/json", "Authorization": "Basic vuton8j6qv0o3mn0kqpkpbdmo6:x"}, data: {deck_id: 'career-deck'} }, function(error, response){
       
-        // Get new Assessment ID         
-        var assessment_id = JSON.parse(response.content).id        
+        // Get new assessment ID         
+        var assessment_id = JSON.parse(response.content).id
+        Session.set("assessment_id", assessment_id)
+
       })
     }
   });
+
+  Template.assessment.helpers({
+    assessment_id: function(){
+      return Session.get("assessment_id");
+    }
+  })
 }
