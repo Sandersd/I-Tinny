0a03c24c6269fd35f2a48239f40a88075fcf2ed4
Leandra Tejedor <lmtejedor@gmail.com>
2015-09-26 22:58:25 -0700

    make front page better

--- i-tinny.js
+++ i-tinny.js
@@ -15,9 +15,13 @@ if (Meteor.isClient) {
       // Prevent default browser form submit
       event.preventDefault();
 
+      //replace info thing with quiz
+      $('.info-container').addClass('is-hidden');
+      $('.white-bg-cover').removeClass('is-hidden');
+
       HTTP.call('POST', 'https://api-sandbox.traitify.com/v1/assessments', {headers: { "Content-Type": "application/json", "Authorization": "Basic vuton8j6qv0o3mn0kqpkpbdmo6:x"}, data: {deck_id: 'core'} }, function(error, response){
-      
-        // Get new assessment ID         
+
+        // Get new assessment ID
         Session.set("assessment_id", JSON.parse(response.content).id)
 
         Traitify.setPublicKey("l6uv37ui1fir0cf7eb519f70le");
