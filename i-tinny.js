e25d52c500fe5c060fd1bb640de4be4d9e75464e
Tom Lee <tlee418@g.ucla.edu>
2015-09-27 12:41:34 -0700

    WIP

--- i-tinny.js
+++ i-tinny.js
@@ -10,14 +10,36 @@ if (Meteor.isClient) {
     BlazeLayout.render('main', { content: "first" });
   })
 
+  Template.calendar.events({
+    "submit .bookFlight": function (event) {
+      event.preventDefault();
+      console.log("TESTING");
+      // call a method here;
+      // Meteor.call("oAuth");
+
+      Meteor.call("bookFlight", function(error, response){
+        Session.set("arrivalCity", response);
+        console.log(Session.get("arrivalCity"));
+      });
+
+      console.log("**************");
+    }
+  });
+
+  Template.calendar.helpers({
+    arrivalCity: function(){
+      return Session.get("arrivalCity");
+    }
+  });
+
   Template.test_page.events({
     "click .new-assessment": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
 
       HTTP.call('POST', 'https://api-sandbox.traitify.com/v1/assessments', {headers: { "Content-Type": "application/json", "Authorization": "Basic vuton8j6qv0o3mn0kqpkpbdmo6:x"}, data: {deck_id: 'core'} }, function(error, response){
-      
-        // Get new assessment ID         
+
+        // Get new assessment ID
         Session.set("assessment_id", JSON.parse(response.content).id)
 
         Traitify.setPublicKey("l6uv37ui1fir0cf7eb519f70le");
@@ -34,7 +56,32 @@ if (Meteor.isClient) {
           })
         })
       })
-    }
-  });
+}
+});
 
+}
+
+
+if (Meteor.isServer) {
+  Meteor.methods({
+    oAuth: function(){
+      HTTP.call('POST', "https://api.test.sabre.com/v2/auth/token", {headers: [{"Content-Type": "application/x-www-form-urlencoded", "Authorization": "Basic VmpFNmNEazViRzlpYVhsbGJtdzBOVzFvWWpwRVJWWkRSVTVVUlZJNlJWaFU6UTNSaGJUTlNXVE09"}]}, function(error, response){
+        console.log(error);
+        console.log("*************");
+        console.log(response);
+      })
+    },
+
+    // VjE6cDk5bG9iaXllbmw0NW1oYjpERVZDRU5URVI6RVhU
+    // Q3RhbTNSWTM=
+
+
+    bookFlight: function(){
+      Meteor.http.get("https://api.test.sabre.com/v2/shop/flights/fares?origin=SFO&lengthofstay=3&location=US&theme=GAMBLING&pointofsalecountry=US&topdestinations=2", {headers: {"Authorization": "Bearer T1RLAQJ2NvikXFGJwHzs4LBwCUySjJVsjhDNHgXTt0IqwRS23JzVG/F3AACgqltkCdUvNVLjlcuodlKEJJi5oRqitfdAkCiIfBKH/og6sI+ZHBIPIpn3ZdudKT+IyMcU5wH0kO0cK5Q8RorgRZixTvivCbiNTfx2Wc3fZ1qxGV+oROA9EDx/2CbQIV4aX9AsmozRIVCELiQ5AhOg2Uh4tqt4NOQrvaMMwV6Go+JK8VVcXIW4JfI38eQbCPl7LYK7WddmclJQ9uNrtnEMFw**", "X-Originating-Ip": "64.245.0.68"}}, function(error, response){
+        reactiveCity = response.data.FareInfo[0].DestinationLocation;
+        console.log(reactiveCity);
+        return reactiveCity;
+      })
+    }
+  })
 }
